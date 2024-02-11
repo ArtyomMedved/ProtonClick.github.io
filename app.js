4c53f69683ff944c102e02c605332d2fb85c2e1a
ArtyomMedved <137563985+ArtyomMedved@users.noreply.github.com>
2024-02-11 15:53:23 +0300

    test-3

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = "";
+
+var BackButton = WebApp.BackButton;
+BackButton.show();
+BackButton.onClick(function() {
+  WebApp.showAlert("Нет пути назад!");
+  BackButton.hide();
+});
+WebApp.onEvent('backButtonClicked', function() {
+  /* код */
+});
